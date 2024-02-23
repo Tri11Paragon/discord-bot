5d5c2934067e32ac8c2e1ee216a441517e65cedc
Brett <brettmaster1@gmail.com>
2024-02-23 14:37:23 -0500

    love

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,18 @@
 int main(int argc, const char** argv)
 {
     blt::arg_parse parser;
-    parser.addArgument(blt::arg_builder("-t", "--token").setDefault("uWU").setAction(blt::arg_action_t::STORE).setHelp("The discord bot token").setRequired().build());
-    
+    parser.addArgument(
+            blt::arg_builder("-t", "--token").setDefault("uWU").setAction(blt::arg_action_t::STORE).setHelp("The discord bot token").setRequired()
+                                             .build());
+
 //    for (int i = 0; i < argc; i++)
 //        BLT_TRACE(argv[i]);
     
     auto args = parser.parse_args(argc, argv);
     
     BLT_TRACE(args.get<std::string>("--token"));
-    BLT_TRACE(argv[argc-1]);
-    dpp::cluster bot(argv[argc-1]);
+    BLT_TRACE(argv[argc - 1]);
+    dpp::cluster bot(argv[argc - 1], dpp::i_default_intents | dpp::i_message_content | dpp::i_all_intents);
     
     bot.on_slashcommand([](auto event) {
         if (event.command.get_command_name() == "ping")
@@ -28,18 +30,27 @@ int main(int argc, const char** argv)
     bot.on_ready([&bot](auto event) {
         if (dpp::run_once<struct register_bot_commands>())
         {
-            bot.global_command_create(
-                    dpp::slashcommand("ping", "Ping pong!", bot.me.id)
-            );
+            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
         }
     });
     
-    bot.on_message_create([&bot](auto event) {
+    bot.on_message_create([&bot](const dpp::message_create_t& event) {
+        if (event.msg.id == bot.me.id)
+            return;
         BLT_TRACE("(%s)> %s", event.msg.author.username.c_str(), event.msg.content.c_str());
-        if (event.msg.author.username == "")
+        for (const dpp::attachment& attach : event.msg.attachments)
         {
-        
+            BLT_INFO("\tAttachment: %s", attach.url.c_str());
         }
+        for (const dpp::embed& embed : event.msg.embeds)
+        {
+            BLT_INFO("\tEmbedding: %s", embed.url.c_str());
+        }
+        //BLT_TRACE_STREAM << event.msg.channel_id.str() << " " << event.msg.channel_id.str() << " "  << "\n";
+//        if (blt::string::toLowerCase(event.msg.author.username) != "autismbot" && blt::string::toLowerCase(event.msg.author.username) != "jewhack")
+//        {
+//            //event.reply("Test response " + event.msg.author.global_name);
+//        }
     });
     
     bot.start(dpp::st_wait);
